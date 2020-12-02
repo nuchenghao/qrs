b0d2d672b1934417a590ae6a9cab49944c021afe
nuchenghao <nuchenghao666@163.com>
2020-12-02 21:45:13 +0800

    2020/12/2

---
+++ exam/24/Debug/obj/gec.d
@@ -0,0 +1,84 @@
+obj/gec.o: srcc/gec.c D:\app_for_qianrushi\exam\24\04_GEC/gec.h \
+ D:\app_for_qianrushi\exam\24\03_MCU/mcu.h \
+ D:\app_for_qianrushi\exam\24\03_MCU\startup/MKL36Z4.h \
+ D:\app_for_qianrushi\exam\24\02_CPU/core_cm0plus.h \
+ D:\app_for_qianrushi\exam\24\02_CPU/core_cmInstr.h \
+ D:\app_for_qianrushi\exam\24\02_CPU/core_cmFunc.h \
+ D:\app_for_qianrushi\exam\24\03_MCU\startup/system_MKL36Z4.h \
+ D:\app_for_qianrushi\exam\24\03_MCU\startup/system_MKL36Z4.h \
+ D:\app_for_qianrushi\exam\24\02_CPU/cpu.h \
+ D:\app_for_qianrushi\exam\24\02_CPU/core_cm0plus.h \
+ D:\app_for_qianrushi\exam\24\02_CPU/core_cmFunc.h \
+ D:\app_for_qianrushi\exam\24\02_CPU/core_cmInstr.h \
+ D:\app_for_qianrushi\exam\24\05_UserBoard/printf.h \
+ D:\app_for_qianrushi\exam\24\03_MCU\MCU_drivers/uart.h \
+ D:\app_for_qianrushi\exam\24\03_MCU\MCU_drivers/flash.h \
+ D:\app_for_qianrushi\exam\24\07_NosPrg/includes.h \
+ D:\app_for_qianrushi\exam\24\05_UserBoard/user.h \
+ D:\app_for_qianrushi\exam\24\05_UserBoard/printf.h \
+ D:\app_for_qianrushi\exam\24\03_MCU\MCU_drivers/gpio.h \
+ D:\app_for_qianrushi\exam\24\05_UserBoard/tm1637.h \
+ D:\app_for_qianrushi\exam\24\05_UserBoard/ws2812.h \
+ D:\app_for_qianrushi\exam\24\05_UserBoard/adlx345.h \
+ D:\app_for_qianrushi\exam\24\03_MCU\MCU_drivers/i2c.h \
+ D:\app_for_qianrushi\exam\24\03_MCU\MCU_drivers/adc.h \
+ D:\app_for_qianrushi\exam\24\05_UserBoard/lcd.h \
+ D:\app_for_qianrushi\exam\24\03_MCU\MCU_drivers/spi.h \
+ D:\app_for_qianrushi\exam\24\03_MCU\MCU_drivers/wdog.h \
+ D:\app_for_qianrushi\exam\24\06_SoftComponent/NumToStr.h
+
+D:\app_for_qianrushi\exam\24\04_GEC/gec.h:
+
+D:\app_for_qianrushi\exam\24\03_MCU/mcu.h:
+
+D:\app_for_qianrushi\exam\24\03_MCU\startup/MKL36Z4.h:
+
+D:\app_for_qianrushi\exam\24\02_CPU/core_cm0plus.h:
+
+D:\app_for_qianrushi\exam\24\02_CPU/core_cmInstr.h:
+
+D:\app_for_qianrushi\exam\24\02_CPU/core_cmFunc.h:
+
+D:\app_for_qianrushi\exam\24\03_MCU\startup/system_MKL36Z4.h:
+
+D:\app_for_qianrushi\exam\24\03_MCU\startup/system_MKL36Z4.h:
+
+D:\app_for_qianrushi\exam\24\02_CPU/cpu.h:
+
+D:\app_for_qianrushi\exam\24\02_CPU/core_cm0plus.h:
+
+D:\app_for_qianrushi\exam\24\02_CPU/core_cmFunc.h:
+
+D:\app_for_qianrushi\exam\24\02_CPU/core_cmInstr.h:
+
+D:\app_for_qianrushi\exam\24\05_UserBoard/printf.h:
+
+D:\app_for_qianrushi\exam\24\03_MCU\MCU_drivers/uart.h:
+
+D:\app_for_qianrushi\exam\24\03_MCU\MCU_drivers/flash.h:
+
+D:\app_for_qianrushi\exam\24\07_NosPrg/includes.h:
+
+D:\app_for_qianrushi\exam\24\05_UserBoard/user.h:
+
+D:\app_for_qianrushi\exam\24\05_UserBoard/printf.h:
+
+D:\app_for_qianrushi\exam\24\03_MCU\MCU_drivers/gpio.h:
+
+D:\app_for_qianrushi\exam\24\05_UserBoard/tm1637.h:
+
+D:\app_for_qianrushi\exam\24\05_UserBoard/ws2812.h:
+
+D:\app_for_qianrushi\exam\24\05_UserBoard/adlx345.h:
+
+D:\app_for_qianrushi\exam\24\03_MCU\MCU_drivers/i2c.h:
+
+D:\app_for_qianrushi\exam\24\03_MCU\MCU_drivers/adc.h:
+
+D:\app_for_qianrushi\exam\24\05_UserBoard/lcd.h:
+
+D:\app_for_qianrushi\exam\24\03_MCU\MCU_drivers/spi.h:
+
+D:\app_for_qianrushi\exam\24\03_MCU\MCU_drivers/wdog.h:
+
+D:\app_for_qianrushi\exam\24\06_SoftComponent/NumToStr.h:
---
+++ exam/24/Debug/obj/lcd.d
@@ -0,0 +1,48 @@
+obj/lcd.o: srcc/lcd.c D:\app_for_qianrushi\exam\24\05_UserBoard/lcd.h \
+ D:\app_for_qianrushi\exam\24\03_MCU/mcu.h \
+ D:\app_for_qianrushi\exam\24\03_MCU\startup/MKL36Z4.h \
+ D:\app_for_qianrushi\exam\24\02_CPU/core_cm0plus.h \
+ D:\app_for_qianrushi\exam\24\02_CPU/core_cmInstr.h \
+ D:\app_for_qianrushi\exam\24\02_CPU/core_cmFunc.h \
+ D:\app_for_qianrushi\exam\24\03_MCU\startup/system_MKL36Z4.h \
+ D:\app_for_qianrushi\exam\24\03_MCU\startup/system_MKL36Z4.h \
+ D:\app_for_qianrushi\exam\24\02_CPU/cpu.h \
+ D:\app_for_qianrushi\exam\24\02_CPU/core_cm0plus.h \
+ D:\app_for_qianrushi\exam\24\02_CPU/core_cmFunc.h \
+ D:\app_for_qianrushi\exam\24\02_CPU/core_cmInstr.h \
+ D:\app_for_qianrushi\exam\24\03_MCU\MCU_drivers/gpio.h \
+ D:\app_for_qianrushi\exam\24\03_MCU\MCU_drivers/spi.h \
+ D:\app_for_qianrushi\exam\24\03_MCU\MCU_drivers/wdog.h \
+ D:\app_for_qianrushi\exam\24\06_SoftComponent/NumToStr.h
+
+D:\app_for_qianrushi\exam\24\05_UserBoard/lcd.h:
+
+D:\app_for_qianrushi\exam\24\03_MCU/mcu.h:
+
+D:\app_for_qianrushi\exam\24\03_MCU\startup/MKL36Z4.h:
+
+D:\app_for_qianrushi\exam\24\02_CPU/core_cm0plus.h:
+
+D:\app_for_qianrushi\exam\24\02_CPU/core_cmInstr.h:
+
+D:\app_for_qianrushi\exam\24\02_CPU/core_cmFunc.h:
+
+D:\app_for_qianrushi\exam\24\03_MCU\startup/system_MKL36Z4.h:
+
+D:\app_for_qianrushi\exam\24\03_MCU\startup/system_MKL36Z4.h:
+
+D:\app_for_qianrushi\exam\24\02_CPU/cpu.h:
+
+D:\app_for_qianrushi\exam\24\02_CPU/core_cm0plus.h:
+
+D:\app_for_qianrushi\exam\24\02_CPU/core_cmFunc.h:
+
+D:\app_for_qianrushi\exam\24\02_CPU/core_cmInstr.h:
+
+D:\app_for_qianrushi\exam\24\03_MCU\MCU_drivers/gpio.h:
+
+D:\app_for_qianrushi\exam\24\03_MCU\MCU_drivers/spi.h:
+
+D:\app_for_qianrushi\exam\24\03_MCU\MCU_drivers/wdog.h:
+
+D:\app_for_qianrushi\exam\24\06_SoftComponent/NumToStr.h:
